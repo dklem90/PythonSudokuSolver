6bf4b3afcd9c6726964873e5d8d9361d14243f3b
Daniel Klem <dklem90@gmail.com>
2020-04-05 16:47:07 -0500

    finished algorithm to solve the board

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
+# import sys
+# sys.setrecursionlimit(3000)
+
 game_board = [
-  [1, 0, 0, 2, 4, 7, 0, 0, 0], 
-  [0, 0, 0, 9, 0, 0, 5, 0, 1], 
-  [0, 8, 0, 5, 0, 0, 7, 0, 0], 
-  [8, 4, 5, 0, 0, 2, 9, 3, 0], 
-  [0, 0, 9, 0, 0, 0, 1, 0, 0], 
-  [0, 7, 1, 6, 0, 0, 2, 5, 4], 
-  [0, 7, 1, 6, 0, 0, 2, 5, 4], 
-  [0, 0, 8, 0, 0, 4, 0, 9, 0], 
-  [0, 0, 0, 3, 1, 9, 0, 0, 5]
+  [2, 7, 3, 1, 0, 0, 0, 5, 0],
+  [5, 0, 0, 0, 0, 9, 3, 0, 1],
+  [0, 0, 8, 0, 0, 0, 2, 6, 7],
+  [7, 0, 0, 0, 0, 0, 0, 0, 0],
+  [9, 0, 1, 5, 2, 7, 8, 0, 6],
+  [0, 0, 0, 0, 0, 0, 0, 0, 5],
+  [3, 4, 9, 0, 0, 0, 5, 0, 0],
+  [6, 0, 7, 8, 0, 0, 0, 0, 3],
+  [0, 2, 0, 0, 0, 3, 6, 1, 9]
 ]
+
+empty_cell_locs_array = []
+
+def standard_input():
+  yield 'Danny'
+
+def findEmptyCell(board):
+  for i in range(len(board)):
+    for j in range(len(board[0])):
+      if board[i][j] == 0:
+        return (i, j)
+  
+  return None
 
 def getSubgrid(row, col):
   sub_grid_no = 0
@@ -39,21 +55,18 @@ def getSubgrid(row, col):
 
   return sub_grid_no
 
-def validCell(row, col, board):
-  cell_value = board[row][col]
-  is_valid = True
-
+def validCell(row, col, board, cell_value): 
   # Check if the row is valid
   for i in range(9):
     if i != col:
       if board[row][i] == cell_value:
-        is_valid = False
+        return False
 
   # Check if the column is valid
   for i in range(9):
     if i != row:
       if board[i][col] == cell_value:
-        is_valid = False
+        return False
 
   # Check if the subgrid is valid
   sub_grid_no = getSubgrid(row, col)
@@ -61,30 +74,82 @@ def validCell(row, col, board):
   if sub_grid_no == 1:
     for i in range(3):
       for k in range(3):
-        if i != row and col != k:
+        if (i == row and col == k) == False:
           if board[i][k] == cell_value:
-            is_valid = False
+            return False
+  elif sub_grid_no == 2:
+    for i in range(3):
+      for k in range(3, 6):
+        if (i == row and col == k) == False:
+          if board[i][k] == cell_value:
+            return False
+  elif sub_grid_no == 3:
+    for i in range(3):
+      for k in range(6, 9):
+        if (i == row and col == k) == False:
+          if board[i][k] == cell_value:
+            return False
+  elif sub_grid_no == 4:
+    for i in range(3, 6):
+      for k in range(3):
+        if (i == row and col == k) == False:
+          if board[i][k] == cell_value:
+            return False
+  elif sub_grid_no == 5:
+    for i in range(3, 6):
+      for k in range(3, 6):
+        if (i == row and col == k) == False:
+          if board[i][k] == cell_value:
+            return False
+  elif sub_grid_no == 6:
+    for i in range(3, 6):
+      for k in range(6, 9):
+        if (i == row and col == k) == False:
+          if board[i][k] == cell_value:
+            return False
+  elif sub_grid_no == 7:
+    for i in range(6, 9):
+      for k in range(3):
+        if (i == row and col == k) == False:
+          if board[i][k] == cell_value:
+            return False
+  elif sub_grid_no == 8:
+    for i in range(6, 9):
+      for k in range(3, 6):
+        if (i == row and col == k) == False:
+          if board[i][k] == cell_value:
+            return False
+  elif sub_grid_no == 9:
+    for i in range(6, 9):
+      for k in range(6, 9):
+        if (i == row and col == k) == False:
+          if board[i][k] == cell_value:
+            return False
 
   # Return if the cell is valid
-  return is_valid
+  return True
 
-def solveBoard(row, col, board):
-  if(len(board[row]) == col):
-    row = row + 1
-    col = 0
+def solveBoard(board):
+  found = findEmptyCell(board)
+  if not found:
+    return True
+  else:
+    row, col = found
 
-  if(board[row][col] != 0):
-    col = col + 1  
+  # # Check that the cell is empty
+  # if checkThatCellIsEmpty(row, col) == False:
+  #   solveBoard(board)
 
+  # Assign a value to the specified cell and validate it
   for i in range(1, 10):
-    board[row][col] = i
-    if(validCell(row, col, board)):
-      print("\n")
-      for row in board:
-        print(row)
-      solveBoard(row, col + 1, board)
-
-  board[row][col] = 0
+    if validCell(row, col, board, i):   
+      board[row][col] = i  
+      if solveBoard(board):
+        return True
+      
+      board[row][col] = 0
+        
+  return False
 
 def startGame(board):
   name = input('What is your name sudoku master? ')
@@ -96,10 +161,20 @@ def startGame(board):
 
   print('Here is today\'s sudoku board: ')
 
-  for row in board:
-    print(row)
+  print("\n")
+  for r in range(9):
+    for c in range(9):
+      print(board[r][c], end="   ")
+    print('\n')
 
-  solveBoard(0, 0, game_board)
+  solveBoard(board)
+
+  print('\nYour solved puzzle!: \n')
+
+  for r in range(9):
+    for c in range(9):
+      print(board[r][c], end="   ")
+    print('\n')
   
 def main():
   startGame(game_board)
